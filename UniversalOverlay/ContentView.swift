f4a40e8a7173bec41efb4257369255df9c4c37fb
Mohammed Rokon Uddin <shoaib.rokon@gmail.com>
2024-10-26 11:54:49 +0600

    Initial Commit

---
+++ UniversalOverlay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UniversalOverlay
+//
+//  Created by Mohammed Rokon Uddin on 10/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UniversalOverlay/UniversalOverlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversalOverlayApp.swift
+//  UniversalOverlay
+//
+//  Created by Mohammed Rokon Uddin on 10/26/24.
+//
+
+import SwiftUI
+
+@main
+struct UniversalOverlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
